60b3865360515563425f51f2ee6e967e3556b014
Joseph Combs <joseph.e.combs@gmail.com>
2015-01-01 19:37:49 -0800

    added faker gem and implemented it in seeds.rb

--- example_apps/rails/db/seeds.rb
+++ example_apps/rails/db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#create seed events
+10.times do |i|
+  Event.create({
+    name: Faker::Name.name,
+    ended_at: Faker::Time.forward(14, :evening),
+    started_at: Faker::Time.backward(14, :morning),
+    user_id: i + 1,
+    address: Faker::Address.street_address,
+    lat: Faker::Address.latitude,
+    lon: Faker::Address.longitude
+  })
+end
+
+#create seed users
+10.times do |i|
+  User.create({
+    device_token: Faker::Number.number(10).to_s,
+  })
+end
+
+#create seed attendances
+10.times do |i|
+  Attendance.create({
+    event_id: i + 1,
+    user_id: i + 1
+  })
+end
+
